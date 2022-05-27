4c502f94df8f15f721b57a5a106872fb6ea29cfe
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-27 12:50:21 -0400

    add

--- js/script.js
+++ js/script.js
@@ -7,26 +7,47 @@
 /**
  * Check servie worker.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-03-JS/sw.js", {
+    scope: "/ICS2O-Unit-6-03-JS/",
   })
 }
 
 ;("use strict")
+
 /**
- * This function calculates area of a parrallelogram.
+ * Get API info.
  */
-function calculate() {
-  // input
-  const length = parseInt(document.getElementById("length-of-pyramid").value)
-  const width = parseInt(document.getElementById("width-of-pyramid").value)
-  const height = parseInt(document.getElementById("height-of-pyramid").value)
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
 
-  // process
-  const volume = (length * width * height) / 3
+/**
+ * Get API info.
+ */
+const getWeather = async (URLAddress) => {
+  try {
+    const request = await fetch(URLAddress);
+    const jsonData = await request.json();
+    var tempK = jsonData.main.temp;
+    var tempC = 0;
+    const feeling = jsonData.weather[0];
+    const image = feeling.icon;
 
-  // output
-  document.getElementById("answers").innerHTML =
-    "Volume is: " + volume.toFixed(2) + " cm³"
-}
+    console.log(jsonData.weather);
+    document.getElementById("api-icon").innerHTML =
+      "<img src='http://openweathermap.org/img/wn/" +
+      image +
+      "@2x.png' alt='Weather Icon' width='10%'><br><h5>";
+    (">");
+
+    // Calculate from Kalvin to Celsius
+    tempC = tempK - 273.15;
+
+    document.getElementById("api-weather").innerHTML =
+      "The current weather is " + tempC.toFixed(2) + " °C";
+  } catch (err) {
+    console.log(err);
+  }
+};
+getWeather(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+);
